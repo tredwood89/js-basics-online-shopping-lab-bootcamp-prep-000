d47bb62c6c388674b3334cd54b92e1e02fa2e51a
tredwood89 <tredwood89@gmail.com>
2017-11-06 22:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,78 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+   var itemPrice = Math.floor(Math.random() * 100)+1
+   var itemName = item
+   var items = {[itemName]: itemPrice}
+
+   cart.push (items)
+
+   console.log  (`${itemName} has been added to your cart.`)
+
+   return cart
+ }
+
+/*
+ function viewCart() {
+   if (cart.length === 0){
+     console.log('Your shopping cart is empty.');
+     return;
+   }
+    var items = [];
+    for(let item of cart){
+      for(let name in item){
+        items.push(`${name} at \$${item[name]}`);
+      }
+    }
+   if (items.length > 1) items[items.length - 1] = "and " + items[items.length - 1];
+   if (items.length > 2){
+      var item_str = items.join(', ');
+   } else {
+     item_str = items.join(' ');
+     }
+       console.log("In your cart, you have " + item_str + ".");
+   }
+*/
+
 
 function viewCart() {
-  // write your code here
+
+  var cartItems = []
+  var intro = "In your cart, you have "
+
+  if (cart.length === 0) {
+    console.log ('Your shopping cart is empty.');
+  }
+  else {
+    for (var i = 0; i < cart.length; i++) {
+      for(var name in cart[i]){
+        if(cart[i].hasOwnProperty(name)){
+
+          cartItems.push (`${name} at $${cart[i][name]}`);
+                          // "apples at $2"
+        } if (cart.length === 1 ){
+
+            console.log (intro + `${cartItems[i]}.`);
+
+        } else if (cart.length === 2 ) {
+
+            console.log ( intro + `${cartItems[0]} and ${cartItems[1]}.`)
+
+        } else if (cart.length >=3) {
+debugger
+            console.log (intro + `${cartItems.slice(0, cart.length-1)}, and ${cartItems[cart.length-1]}.`)
+        }
+      }
+    }
+  }
 }
 
+
+      //  var fullCart = cartItems.slice(i, cart.length-1)
+      //  var lastItem = cartItems[cart.length-1]
+
 function total() {
-  // write your code here
+
 }
 
 function removeFromCart(item) {
@@ -26,5 +89,5 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  // write your code here}
 }
